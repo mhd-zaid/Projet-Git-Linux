44e3125d9da3796b984e8f5b96659617ea7231f5
jugo92 <jugzek92@outlook.fr>
2022-06-16 10:34:27 +0200

    feature create FILENAME  complete

--- Scriptblog.sh
+++ Scriptblog.sh
@@ -5,8 +5,19 @@
 
 if [  -e FILENAME.html ]
   then
-          echo 'Error : Le fichier FILENAME existe deja'
+          echo 'Error : Une erreur est survenu'
           exit 0
   else
-          touch FILENAME.html
+          touch FILENAME.html  2>> errors.txt
+          cat PageHtml5.txt >> FILENAME.html
+fi
+if [ -s errors.txt ]
+   then
+         echo "Une erreur s'est produite"
+         exit 0
+fi
+if [ ! -s FILENAME.html  ]
+   then
+           echo 'Error : Le ficher FILENAME est vide'
+           exit 0
 fi
