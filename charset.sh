0b01c8e28a0771794071ac998a6f6980cfefdb99
kidax <axel.halifa0@gmail.com>
2022-06-16 10:36:00 +0200

    Début de code de  l'option update-charset

---
+++ charset.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+charset='charset='
+encoding=$1
+fileName=$2
+ 
+echo $encoding
+if [ -z $encoding ]
+then
+	echo "La valeur d'encoding est vide"
+fi
+
+if [ -z $fileName ] 
+then 
+	echo "Vous n'avez pas renseigné de nom fichier"
+fi 
+ 
+if grep -q $charset $fileName
+then
+	sed -i 's/.*<meta charset=.*/<meta charset="'$encoding'">/g' $fileName
+else
+	sed -i '/<head>/a <meta charset="'$encoding'">' $fileName
+fi
+
+
